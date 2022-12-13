d8c6f4f81bfb3c92b4c5553e92d386c8a31c44ad
Ziolo1999 <kj.ziolo@student.uw.edu.pl>
2022-12-13 22:49:28 +0100

    update: evaluation

--- retrival_system/web_app/evaluation/evaluation_static.py
+++ retrival_system/web_app/evaluation/evaluation_static.py
@@ -16,42 +16,25 @@ q4 = "water found on Mars"
 q5 = "El Chapo escapes prison"
 queries = [q1, q2, q3, q4, q5]
 
-def run_all(queries):
-    # Load data:
-    loaded_data = load_data(MODEL_COMPOMENTS_PATH, TWEETS_DATAFRAME_PATH)
-    # Load sets of relevant queries
-    relevant_queries = load_relevant_queries(RELEVANT_QUERIES_PATH)
-    # Get ids lists of relevant query and their totals
-    relevant_doc_dict, total_relevant = get_tweet_id(queries, loaded_data, relevant_queries)
-    # Run all models
-    results_dict = running_results(queries, loaded_data, total_relevant)
-    # Compare results with the results sets
-    relevance_vector = benchmarking(results_dict, relevant_doc_dict)
-    # Calculate MAP results for each model
-    map_results = mean_average_precision(relevance_vector, total_relevant, queries)
-    # Plot PR-curves 
-    pr_plots = pr_curve(relevance_vector, total_relevant, queries)
 
-    return map_results
-
-
-def id_func(result):
-    id = loaded_data["all_tweets"]["id"][loaded_data["all_tweets"]["tweet"] == result]
-    return int(id)
-
+# Function which tweets ids to relevant documents
 def get_tweet_id(queries, loaded_data, relevant_queries):
     relevant_dict = {}
     total_relevant = {}
     for query in queries:
         relevant_set = relevant_queries[query].dropna()
-        relevant_dict[query] = list(map(id_func, relevant_set))
+        relevant_dict[query] = list(map(lambda x: 
+                                                int(loaded_data["all_tweets"]["id"][loaded_data["all_tweets"]["tweet"] == x])
+                                                ,relevant_set))
         total_relevant[query] = len(relevant_dict[query])
     return relevant_dict, total_relevant
 
+# Function which loads sets of relevant documents
 def load_relevant_queries(RELEVANT_QUERIES_PATH):
     relevant_queries = pd.read_excel(RELEVANT_QUERIES_PATH)
     return relevant_queries
 
+# Function which runs results from all models
 def running_results(queries, loaded_data, total_relevant):
     results_dict = {}
     for query in queries:
@@ -82,16 +65,19 @@ def running_results(queries, loaded_data, total_relevant):
                     lam=0.6, 
                     N=n)
 
-        results_dict[query] = {"vsm": vsm_results, "bm25": bm25_results, "mm": mm_results}
+        rocchio_results = rocchio_data[query]
+
+        results_dict[query] = {"vsm": vsm_results, "bm25": bm25_results, "mm": mm_results, "rocchio": rocchio_results}
 
     return results_dict
 
+# Function which compares system's results with relevance sets
 def benchmarking(results_dict, relevant_dict):
     relevance_dict = {}
     for query in queries:
         n = len(results_dict[query]["vsm"])
         relevant_set = relevant_dict[query]
-        query_dict = {"vsm": 0, "bm25": 0, "mm": 0}
+        query_dict = {"vsm": 0, "bm25": 0, "mm": 0, "rocchio": 0}
 
         for model in query_dict.keys():
             relevance_vector = np.zeros(n)
@@ -124,11 +110,11 @@ def average_precision(arr, relevant):
 
 # Function which gives MAP results for each model (based on the relevant documents presented in results which are pointed out by user)
 def mean_average_precision(relevance_vector, total_relevant, queries):
-    model_list = ["vsm", "bm25", "mm"]
-    positive_matrix = np.zeros([len(queries), 3])
+    model_list = ["vsm", "bm25", "mm", "rocchio"]
+    positive_matrix = np.zeros([len(queries), 4])
     row_counter = 0
     for query in queries:
-        for model_id in range(3):
+        for model_id in range(4):
             positive_matrix[row_counter][model_id] = average_precision(relevance_vector[query][model_list[model_id]], total_relevant[query])
         row_counter += 1
     return np.apply_along_axis(np.mean, 0, positive_matrix)
@@ -150,64 +136,91 @@ def precision_recall_matrix(arr, relevant):
 
 # Provides PR curves for each query comparing models
 def pr_curve(relevance_vector, total_relevant, queries):
-    model_list = ["vsm", "bm25", "mm"]
+    model_list = ["vsm", "bm25", "mm", "rocchio"]
     pr_matrix = {query: {model: 0 
                     for model in range(len(model_list))}
                 for query in range(len(queries))}
     row_counter = 0
     for query_id in range(len(queries)):
-        for model_id in range(3):
+        for model_id in range(4):
             pr_matrix[query_id][model_id] = precision_recall_matrix(relevance_vector[queries[query_id]][model_list[model_id]], total_relevant[queries[query_id]])
         row_counter += 1
 
-    fig, (ax1,ax2,ax3,ax4,ax5) = plt.subplots(5, figsize=(50,0))
-    ax1.plot(pr_matrix[0][0][:,0], pr_matrix[0][0][:,1], '-o', 
+    fig, ax = plt.subplots(3,2)
+    ax[0,0].plot(pr_matrix[0][0][:,0], pr_matrix[0][0][:,1], '-o', 
                 pr_matrix[0][1][:,0], pr_matrix[0][1][:,1], '-o', 
-                pr_matrix[0][2][:,0], pr_matrix[0][2][:,1], '-o')
-    ax1.set_xlim([-0.1, 1.1])
-    ax1.set_ylim([-0.1, 1.1])
-    ax1.legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model"])
-    ax1.set_title(q1)
-    ax1.set_xlabel("Recall")
+                pr_matrix[0][2][:,0], pr_matrix[0][2][:,1], '-o',
+                pr_matrix[0][3][:,0], pr_matrix[0][3][:,1], '-o')
+    ax[0,0].set_xlim([-0.1, 1.1])
+    ax[0,0].set_ylim([-0.1, 1.1])
+    ax[0,0].legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model", "Rocchio"])
+    ax[0,0].set_title(q1)
+    ax[0,0].set_xlabel("Recall")
+    ax[0,0].set_ylabel("Precision")
 
-    ax2.plot(pr_matrix[1][0][:,0], pr_matrix[1][0][:,1], '-o', 
+    ax[0,1].plot(pr_matrix[1][0][:,0], pr_matrix[1][0][:,1], '-o', 
                 pr_matrix[1][1][:,0], pr_matrix[1][1][:,1], '-o', 
-                pr_matrix[1][2][:,0], pr_matrix[1][2][:,1], '-o')
-    ax2.set_xlim([-0.1, 1.1])                
-    ax2.set_ylim([-0.1, 1.1])
-    ax2.legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model"])
+                pr_matrix[1][2][:,0], pr_matrix[1][2][:,1], '-o',
+                pr_matrix[1][3][:,0], pr_matrix[1][3][:,1], '-o')
+    ax[0,1].set_xlim([-0.1, 1.1])                
+    ax[0,1].set_ylim([-0.1, 1.1])
+    ax[0,1].legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model", "Rocchio"])
+    ax[0,1].set_title(q2)
+    ax[0,1].set_xlabel("Recall")
+    ax[0,1].set_ylabel("Precision")
 
-    ax3.plot(pr_matrix[2][0][:,0], pr_matrix[2][0][:,1], '-o', 
+    ax[1,0].plot(pr_matrix[2][0][:,0], pr_matrix[2][0][:,1], '-o', 
                 pr_matrix[2][1][:,0], pr_matrix[2][1][:,1], '-o', 
-                pr_matrix[2][2][:,0], pr_matrix[2][2][:,1], '-o')
-    ax3.set_xlim([-0.1, 1.1])
-    ax3.set_ylim([-0.1, 1.1])
-    ax3.legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model"])
+                pr_matrix[2][2][:,0], pr_matrix[2][2][:,1], '-o',
+                pr_matrix[2][3][:,0], pr_matrix[2][3][:,1], '-o')
+    ax[1,0].set_xlim([-0.1, 1.1])
+    ax[1,0].set_ylim([-0.1, 1.1])
+    ax[1,0].legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model", "Rocchio"])
+    ax[1,0].set_title(q3)
+    ax[1,0].set_xlabel("Recall")
+    ax[1,0].set_ylabel("Precision")
 
-    ax4.plot(pr_matrix[3][0][:,0], pr_matrix[3][0][:,1], '-o', 
+    ax[1,1].plot(pr_matrix[3][0][:,0], pr_matrix[3][0][:,1], '-o', 
             pr_matrix[3][1][:,0], pr_matrix[3][1][:,1], '-o', 
-            pr_matrix[3][2][:,0], pr_matrix[3][2][:,1], '-o')
-    ax4.set_xlim([-0.1, 1.1])
-    ax4.set_ylim([-0.1, 1.1])
-    ax4.legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model"])
+            pr_matrix[3][2][:,0], pr_matrix[3][2][:,1], '-o',
+            pr_matrix[3][3][:,0], pr_matrix[3][3][:,1], '-o')
+    ax[1,1].set_xlim([-0.1, 1.1])
+    ax[1,1].set_ylim([-0.1, 1.1])
+    ax[1,1].legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model", "Rocchio"])
+    ax[1,1].set_title(q4)
+    ax[1,1].set_xlabel("Recall")
+    ax[1,1].set_ylabel("Precision")
 
-    ax5.plot(pr_matrix[4][0][:,0], pr_matrix[4][0][:,1], '-o', 
+    ax[2,0].plot(pr_matrix[4][0][:,0], pr_matrix[4][0][:,1], '-o', 
             pr_matrix[4][1][:,0], pr_matrix[4][1][:,1], '-o', 
-            pr_matrix[4][2][:,0], pr_matrix[4][2][:,1], '-o')
-    ax5.set_xlim([-0.1, 1.1])
-    ax5.set_ylim([-0.1, 1.1])
-    ax5.legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model"])
+            pr_matrix[4][2][:,0], pr_matrix[4][2][:,1], '-o',
+            pr_matrix[4][3][:,0], pr_matrix[4][3][:,1], '-o')
+    ax[2,0].set_xlim([-0.1, 1.1])
+    ax[2,0].set_ylim([-0.1, 1.1])
+    ax[2,0].legend(['Vector Space Model', 'BestMatch25 Model', "Mixture Model", "Rocchio"])
+    ax[2,0].set_title(q5)
+    ax[2,0].set_xlabel("Recall")
+    ax[2,0].set_ylabel("Precision")
 
     return plt.show()
 
-model_list = ["vsm", "bm25", "mm"]
-positive_matrix = np.zeros([len(queries), 3])
-row_counter = 0
-for query in queries:
-    for model_id in range(3):
-        positive_matrix[row_counter][model_id] = average_precision(relevance_vector[query][model_list[model_id]], total_relevant[query])
-    row_counter += 1
-pm = pd.DataFrame(positive_matrix)
-pm.to_excel("evaluation/avg_prec.xlsx")
-indx = results_dict[q4]["vsm"].index
-print(loaded_data["all_tweets"]["tweet"].iloc[list(indx)])
+# Run all components
+def run_all(queries):
+    # Load data:
+    loaded_data = load_data(MODEL_COMPOMENTS_PATH, TWEETS_DATAFRAME_PATH)
+    # Load sets of relevant queries
+    relevant_queries = load_relevant_queries(RELEVANT_QUERIES_PATH)
+    # Get ids lists of relevant query and their totals
+    relevant_doc_dict, total_relevant = get_tweet_id(queries, loaded_data, relevant_queries)
+    # Run all models
+    results_dict = running_results(queries, loaded_data, total_relevant)
+    # Compare results with the results sets
+    relevance_vector = benchmarking(results_dict, relevant_doc_dict)
+    # Calculate MAP results for each model
+    map_results = mean_average_precision(relevance_vector, total_relevant, queries)
+    # Plot PR-curves 
+    pr_plots = pr_curve(relevance_vector, total_relevant, queries)
+
+    return map_results
+
+
